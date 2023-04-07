7ec182d980a04f2d64f1edec60cfd3bd620bbc0f
wwwik <wwwik@DESKTOP-IO94L1F>
2023-04-08 01:07:38 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(854, 806);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(118, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Раскрасить грани";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(897, 829);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(854, 858);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(854, 910);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(854, 884);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 961);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Drawing;
+
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public struct Point3D
+    {
+        public Point3D(double x, double y, double z)
+        {
+            X = x; Y = y; Z = z;
+        }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; } 
+    }
+
+    public partial class Form1 : Form
+    {
+        double degree_x;
+        double degree_y;
+        double degree_z;
+        int counter;
+        double edge_size;
+        bool needPaint;
+        double multiplier = 0.01;
+
+        Graphics g;
+        List<Point3D> Vertexes;
+        List<(int, int)> Edges;
+        List<(int, int, int)> Surfaces;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            g = CreateGraphics();
+
+            degree_x = 0;
+            degree_y = 0;
+            degree_z = 0;
+            edge_size = 500;
+            counter = 0;
+            needPaint = false;
+
+            Vertexes = new List<Point3D>(){
+                new Point3D(-edge_size*1/2, -edge_size*Math.Sqrt(3)/6, -edge_size*Math.Sqrt(6)/12),
+                new Point3D(edge_size * 1 / 2, -edge_size * Math.Sqrt(3) / 6, -edge_size * Math.Sqrt(6) / 12),
+                new Point3D(0, edge_size * Math.Sqrt(3) / 3, -edge_size * Math.Sqrt(6) / 12),
+                new Point3D(0, 0, edge_size * Math.Sqrt(6) / 6)
+        };
+
+            Edges = new List<(int, int)>() {
+                (1, 0),
+                (2, 0),
+                (3, 0),
+                (1, 2),
+                (1, 3),
+                (2, 3),
+            };
+
+            Surfaces = new List<(int, int, int)>(){
+                (0, 1, 2),
+                (0, 3, 1),
+                (3, 2, 1),
+                (0, 3, 2),
+            };
+
+
+        }
+        void myDraw()
+        {
+
+            g.Clear(Color.White);
+            List<Point3D> stepVertex = new List<Point3D>();
+            for (int i = 0; i < Vertexes.Count; i++)
+            {
+                double x = Vertexes[i].X;
+                double y = Vertexes[i].Y;
+                double z = Vertexes[i].Z;
+
+
+                double help_y1 = y * Math.Cos(degree_x * multiplier * counter) - z * Math.Sin(degree_x * multiplier * counter);
+                double help_z1 = y * Math.Sin(degree_x * multiplier * counter) + z * Math.Cos(degree_x * multiplier * counter);
+
+                double help_x1 = help_z1 * Math.Sin(degree_y * multiplier * counter) + x * Math.Cos(degree_y * multiplier * counter);
+                double help_z2 = help_z1 * Math.Cos(degree_y * multiplier * counter) - x * Math.Sin(degree_y * multiplier * counter);
+
+                double help_x2 = help_x1 * Math.Cos(degree_z * multiplier * counter) - help_y1 * Math.Sin(degree_z * multiplier * counter);
+                double help_y2 = help_x1 * Math.Sin(degree_z * multiplier * counter) + help_y1 * Math.Cos(degree_z * multiplier * counter);
+
+                stepVertex.Add(new Point3D(help_x2, help_y2, help_z2));
+            }
+
+
+            for (int i = 0; i < Edges.Count; i++)
+            {
+                g.DrawLine(
+                    Pens.Black,
+                    (float)stepVertex[Edges[i].Item1].X + 500,
+                    (float)stepVertex[Edges[i].Item1].Y + 500,
+                    (float)stepVertex[Edges[i].Item2].X + 500,
+                    (float)stepVertex[Edges[i].Item2].Y + 500
+                );
+            }
+
+            if (needPaint)
+            {
+                for (int i = 0; i < Surfaces.Count; i++)
+                {
+
+                    switch (i)
+                    {
+                        case 0:
+                            g.FillPolygon(Brushes.Red, new Point[3] {
+                            new Point((int)stepVertex[Surfaces[i].Item1].X + 500 , (int)stepVertex[Surfaces[i].Item1].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item2].X + 500, (int)stepVertex[Surfaces[i].Item2].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item3].X + 500, (int)stepVertex[Surfaces[i].Item3].Y + 500),
+                        });
+                            break;
+                        case 1:
+                            g.FillPolygon(Brushes.Green, new Point[3] {
+                            new Point((int)stepVertex[Surfaces[i].Item1].X + 500, (int)stepVertex[Surfaces[i].Item1].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item2].X + 500, (int)stepVertex[Surfaces[i].Item2].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item3].X + 500, (int)stepVertex[Surfaces[i].Item3].Y + 500),
+                        });
+                            break;
+                        case 2:
+                            g.FillPolygon(Brushes.Blue, new Point[3] {
+                            new Point((int)stepVertex[Surfaces[i].Item1].X + 500, (int)stepVertex[Surfaces[i].Item1].Y  + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item2].X + 500, (int)stepVertex[Surfaces[i].Item2].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item3].X + 500, (int)stepVertex[Surfaces[i].Item3].Y + 500),
+                        });
+                            break;
+                        case 3:
+                            g.FillPolygon(Brushes.Yellow, new Point[3] {
+                            new Point((int)stepVertex[Surfaces[i].Item1].X + 500, (int)stepVertex[Surfaces[i].Item1].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item2].X + 500, (int)stepVertex[Surfaces[i].Item2].Y + 500),
+                            new Point((int)stepVertex[Surfaces[i].Item3].X + 500, (int)stepVertex[Surfaces[i].Item3].Y + 500),
+                        });
+                            break;
+                    };
+                }
+            }
+        }
+        
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++;
+            myDraw();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            needPaint = needPaint == true ? false : true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            degree_x = (double)numericUpDown1.Value;
+            
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            degree_y = (double)numericUpDown2.Value;   
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            degree_z = (double)numericUpDown3.Value;
+        }
+    }
+}
